99d0f3fa4ad17dee7caf31575a3730d428781b04
JFrost135y <33869660+JFrost135y@users.noreply.github.com>
2018-11-05 09:30:28 -0500

    Add project files.

---
+++ Name Generator Winform/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Name_Generator_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Generate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbox_FirstName = new System.Windows.Forms.TextBox();
+            this.txtbox_LastName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Generate
+            // 
+            this.btn_Generate.Location = new System.Drawing.Point(181, 126);
+            this.btn_Generate.Name = "btn_Generate";
+            this.btn_Generate.Size = new System.Drawing.Size(83, 23);
+            this.btn_Generate.TabIndex = 0;
+            this.btn_Generate.Text = "Generate";
+            this.btn_Generate.UseVisualStyleBackColor = true;
+            this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last";
+            // 
+            // txtbox_FirstName
+            // 
+            this.txtbox_FirstName.Location = new System.Drawing.Point(181, 22);
+            this.txtbox_FirstName.Name = "txtbox_FirstName";
+            this.txtbox_FirstName.Size = new System.Drawing.Size(211, 22);
+            this.txtbox_FirstName.TabIndex = 3;
+            // 
+            // txtbox_LastName
+            // 
+            this.txtbox_LastName.Location = new System.Drawing.Point(181, 66);
+            this.txtbox_LastName.Name = "txtbox_LastName";
+            this.txtbox_LastName.Size = new System.Drawing.Size(211, 22);
+            this.txtbox_LastName.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 194);
+            this.Controls.Add(this.txtbox_LastName);
+            this.Controls.Add(this.txtbox_FirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Generate);
+            this.Name = "Form1";
+            this.Text = "My Name Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Generate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbox_FirstName;
+        private System.Windows.Forms.TextBox txtbox_LastName;
+    }
+}
+
---
+++ Name Generator Winform/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Name_Generator_Winform
+{
+    public partial class Form1 : Form
+    {
+        public string[] FirstNames = {"Milan", "Josh", "Daniel", "Mason", "Max", "Levi" };
+
+        public string[] LastNames = { "Bryant", "James", "Iverson", "O'Neal", "Durant", "Jordan" };
+
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Generate_Click(object sender, EventArgs e)
+        {
+            int indexFirstName = rand.Next(FirstNames.Length);
+            int indexLastName = rand.Next(LastNames.Length);
+
+            this.txtbox_FirstName.Text = FirstNames[indexFirstName];
+            this.txtbox_LastName.Text = LastNames[indexLastName];
+        }
+    }
+}
